555da7b3e7767e8ef69720d996904a4b230d50b4
Aliaksandr Tserashkou <atserashkou@gmail.com>
2025-08-14 20:34:58 +0200

    Initial project

---
+++ FavBlueTests/FavBlueTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import FavBlue
+
+struct FavBlueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
